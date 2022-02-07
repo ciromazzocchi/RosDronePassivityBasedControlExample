327ab187e0e858aad328824b6d84f4ae6f6f6b12
Ciro Mazzocchi <ciro.mazzocchi@outlook.com>
2022-02-07 19:37:00 +0100

    Updated odometry node

--- src/Nodes/odometry_node.cpp
+++ src/Nodes/odometry_node.cpp
@@ -17,37 +17,40 @@ Differentiator<Eigen::Vector3d> etaFilter;
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& odom) {
     
-    tf::Matrix3x3 RNed;
-    RNed.setEulerYPR(M_PI/2,0,M_PI);
+    //tf::Matrix3x3 RNed;
+    //RNed.setEulerYPR(M_PI/2,0,M_PI);
 
-    tf::Quaternion q(odom->pose.pose.orientation.x, odom->pose.pose.orientation.y, odom->pose.pose.orientation.z,  odom->pose.pose.orientation.w);
-    tf::Matrix3x3 Rb(q);
-    tf::Matrix3x3 RbNed = RNed*Rb*(RNed.transpose());
+    //tf::Quaternion q(odom->pose.pose.orientation.x, odom->pose.pose.orientation.y, odom->pose.pose.orientation.z,  odom->pose.pose.orientation.w);
+    //tf::Matrix3x3 RbNED(q);
+    Eigen::Quaterniond q;
+    q.x() = odom->pose.pose.orientation.x;
+    q.y() = odom->pose.pose.orientation.y;
+    q.z() = odom->pose.pose.orientation.z;
+    q.w() = odom->pose.pose.orientation.w;
+    Eigen::Matrix3d RbNed = q.normalized().toRotationMatrix();
+    //tf::Matrix3x3 RbNed = RNed*Rb*(RNed.transpose());
 
-    Eigen::Matrix3d Rned;
-    Rned << RNed[0][0], RNed[0][1], RNed[0][2],
-            RNed[1][0], RNed[1][1], RNed[1][2],
-            RNed[2][0], RNed[2][1], RNed[2][2];
 
-    Eigen::Vector3d p = Eigen::Vector3d( odom->pose.pose.position.x,
-        odom->pose.pose.position.y, odom->pose.pose.position.z);
-    p = Rned*p;
+    Eigen::Vector3d p = Eigen::Vector3d(        odom->pose.pose.position.x,
+                                                odom->pose.pose.position.y,
+                                                odom->pose.pose.position.z);
 
-    Eigen::Vector3d eta;
-    RbNed.getRPY(eta[0],eta[1],eta[2]);
+    Eigen::Vector3d eta = RbNed.eulerAngles(2,1,0);
 
-    Eigen::Vector3d pDot = Eigen::Vector3d( odom->twist.twist.linear.x,
-        odom->twist.twist.linear.y, odom->twist.twist.linear.z);
-    pDot = Rned*pDot;
+    Eigen::Vector3d pDot = Eigen::Vector3d(     odom->twist.twist.linear.x,
+                                                odom->twist.twist.linear.y,
+                                                odom->twist.twist.linear.z);
+    pDot = RbNed*pDot;
 
-    Eigen::Vector3d etaDot = Eigen::Vector3d ( odom->twist.twist.angular.x,
-        odom->twist.twist.angular.y, odom->twist.twist.angular.z);
-    etaDot = Rned*etaDot;
+    Eigen::Vector3d etaDot = Eigen::Vector3d (  odom->twist.twist.angular.x,
+                                                odom->twist.twist.angular.y,
+                                                odom->twist.twist.angular.z);
+    etaDot = RbNed*etaDot;
 
     Eigen::Vector3d pDotDot     = pFilter.getDifferentiatoredValue(pDot);
     Eigen::Vector3d etaDotDot   = etaFilter.getDifferentiatoredValue(etaDot);
 
-    msg.header.stamp = ros::Time::now();
+    msg.header.stamp = odom->header.stamp;
     msg.header.frame_id = "worldNED";
 
     tf::vectorEigenToMsg(p,msg.pose.linear);
