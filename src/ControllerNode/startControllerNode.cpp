0a367eea0823d8a1e3685dbe610291b873861466
Ciro Mazzocchi <ciro.mazzocchi@outlook.com>
2022-02-19 09:13:52 +0100

    removed unused files

--- src/ControllerNode/startControllerNode.cpp
+++
@@ -1,9 +0,0 @@
-#include "ros/ros.h"
-#include "./Controller.hpp"
-
-int main(int argc, char **argv){
-    ros::init(argc, argv, "passivityController");
-    Controller ctrl;
-    ctrl.run();
-    return 0;
-}
