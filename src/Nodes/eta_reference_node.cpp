0994a9e719a5d437b20201b7bc7a0198ab0c062f
Ciro Mazzocchi <ciro.mazzocchi@outlook.com>
2022-02-19 10:40:03 +0100

    Cleaned code and added error calculation

--- src/Nodes/eta_reference_node.cpp
+++ src/Nodes/eta_reference_node.cpp
@@ -1,5 +1,4 @@
 #include "ros/ros.h"
-#include "geometry_msgs/Wrench.h"
 #include "quad_control/UavState.h"
 #include "quad_control/DesiredTrajectory.h"
 #include "nav_msgs/Odometry.h"
@@ -12,46 +11,46 @@
 #include <eigen_conversions/eigen_msg.h>
 
 Differentiator<Eigen::Vector3d> eta_dot_filter;
-Differentiator<Eigen::Vector3d> eta_dot_dot_filter;
+Differentiator<Eigen::Vector3d> etaDot_dot_filter;
 
-ros::Subscriber eta_ref_sub_1, eta_ref_sub_2, eta_ref_sub_3;
+ros::Subscriber eta_ref_sub;
 ros::Publisher  eta_ref_pub;
 
-double m, psi_d, v;
-Eigen::Vector3d eta,eta_dot;
+double m, v;
 
-void odom_cb(const nav_msgs::Odometry::ConstPtr& odom_msg) {
-    eta << odom_msg->pose.pose.orientation.x,
-        odom_msg->pose.pose.orientation.y,
-        odom_msg->pose.pose.orientation.z;
-    
-    eta_dot << odom_msg->twist.twist.angular.x,
-        odom_msg->twist.twist.angular.y,
-        odom_msg->twist.twist.angular.z;
-}
-
-void traj_cb(const quad_control::DesiredTrajectory::ConstPtr& traj_msg) {
-    psi_d = traj_msg->position.yaw;
-}
-
-void mu_hat_cb(const geometry_msgs::Vector3Stamped::ConstPtr& mu_hat_msg) {
+void mu_hat_cb(const quad_control::UavState::ConstPtr& mu_hat_msg) {
     Eigen::Vector3d mu_hat;
-    tf::vectorMsgToEigen(mu_hat_msg->vector,mu_hat);
+    tf::vectorMsgToEigen(mu_hat_msg->mu_hat,mu_hat);
+
+    mu_hat[2] = mu_hat[2] - 9.81;
+    
+    double psi_d = mu_hat_msg->psi_d;
 
     double phi_d = asin( ( mu_hat(1)*cos(psi_d) - mu_hat(0)*sin(psi_d) ) /
                     ( sqrt( mu_hat.transpose() * mu_hat) ));
     
-    double theta_d = atan2( mu_hat(0)*cos(psi_d) + mu_hat(1)*sin(psi_d), mu_hat(2));
+    double theta_d = atan( ( mu_hat(0)*cos(psi_d) + mu_hat(1)*sin(psi_d) ) / mu_hat(2));
 
     Eigen::Vector3d eta_d = Eigen::Vector3d(phi_d,theta_d,psi_d);
-    Eigen::Vector3d eta_dot_d = eta_dot_filter.getDifferentiatoredValue(eta_d);
-    Eigen::Vector3d eta_dot_dot_d = eta_dot_dot_filter.getDifferentiatoredValue(eta_dot_d);
+    Eigen::Vector3d etaDot_d = eta_dot_filter.getDifferentiatoredValue(eta_d);
+    Eigen::Vector3d etaDotDot_d = etaDot_dot_filter.getDifferentiatoredValue(etaDot_d);
 
     quad_control::UavState msg;
-    msg.header = mu_hat_msg->header;
-    tf::vectorEigenToMsg(eta_d,msg.pose);
-    tf::vectorEigenToMsg(eta_dot_d,msg.twist);
-    tf::vectorEigenToMsg(eta_dot_dot_d,msg.wrench);
+    msg = *mu_hat_msg;
+    tf::vectorEigenToMsg(eta_d,msg.pose_d.angular);
+    tf::vectorEigenToMsg(etaDot_d,msg.twist_d.angular);
+    tf::vectorEigenToMsg(etaDotDot_d,msg.wrench_d.angular);
+
+    Eigen::Vector3d e,eDot;
+    e = Eigen::Vector3d(mu_hat_msg->pose.angular.x, mu_hat_msg->pose.angular.y,
+        mu_hat_msg->pose.angular.z) - eta_d;
+    
+    eDot = Eigen::Vector3d(mu_hat_msg->twist.angular.x, mu_hat_msg->twist.angular.y,
+        mu_hat_msg->twist.angular.z) - etaDot_d;
+    
+    tf::vectorEigenToMsg(e,msg.error_pose.angular);
+    tf::vectorEigenToMsg(eDot,msg.error_twist.angular);
+
     eta_ref_pub.publish(msg);
 }
 
@@ -60,21 +59,16 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "eta_reference_node");
     ros::NodeHandle nh("~");
 
-    double Ts   = nh.param<double>("Ts", 0.001);
-    m           = nh.param<double>("mass", 0.1);
-    eta         = Eigen::Vector3d::Zero();
-    eta_dot     = Eigen::Vector3d::Zero();
-    psi_d = 0;
+    m       = nh.param<double>("mass", 0.1);
     
-    double kf = nh.param<double>("kf", 100);
-    eta_dot_filter.DifferentiatorInit( kf, Ts );
-    eta_dot_dot_filter.DifferentiatorInit( kf, Ts );
+    double kf_d = nh.param<double>("kf_d", 100);
+    double kf_dd = nh.param<double>("kf_dd", 100);
+    eta_dot_filter.DifferentiatorInit( kf_d, 0.001 );
+    etaDot_dot_filter.DifferentiatorInit( kf_dd, 0.001 );
 
-    eta_ref_sub_1 = nh.subscribe("/odometry", 1, odom_cb);
-    eta_ref_sub_2 = nh.subscribe("/eta_d", 1, traj_cb);
-    eta_ref_sub_3 = nh.subscribe("/mu_hat", 1, mu_hat_cb);
-    eta_ref_pub   = nh.advertise<quad_control::UavState>("/eta_reference", 1);
-    ros::Rate rate(1/Ts);
+    eta_ref_sub = nh.subscribe("/mu_hat", 1, mu_hat_cb);
+    eta_ref_pub   = nh.advertise<quad_control::UavState>("/eta_ref", 1);
+    ros::Rate rate(1000);
     
     ros::Duration(0.02).sleep();
 
