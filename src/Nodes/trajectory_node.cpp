183a65410d35249c46a95d83a5f3f41e965f172c
Ciro Mazzocchi <ciro.mazzocchi@outlook.com>
2022-03-23 19:36:17 +0100

    Solved local minima problem

--- src/Nodes/trajectory_node.cpp
+++ src/Nodes/trajectory_node.cpp
@@ -18,12 +18,17 @@
 using namespace Eigen;
 
 std::list<Vector3d> waypoints_list;
+std::list<Vector3d> skeleton_point;
+std::list<Vector3d> trapping_point;
+std::list<Vector3d> object_point;
 
 std::list<Vector3d>::iterator actual_point;
 
-Vector3d p, p_dot, f_rep;
+Vector3d p, p_dot;
 
 double threshold, ka, rate, psi, k_rep, rng_infl;
+double T_local;
+bool flag_local_minimum;
 
 Differentiator<Vector3d> acceleration_filter;
 
@@ -32,9 +37,84 @@ ros::Publisher  traj_p_pub, traj_yaw_pub;
 
 Matrix3d R_NED;
 
-void gaz_cb(const gazebo_msgs::ModelStates::ConstPtr& msg) {
-    f_rep = Vector3d::Zero();
+Vector3d getFAtt(Vector3d p_uav) {
+    Vector3d error = waypoints_list.front()-p_uav;
+    Vector3d f_att = error * ka / (error.norm() > 1 ? error.norm() : 1 );
+    return f_att;
+}
 
+Vector3d getFRep (Vector3d p_uav) {
+    Vector3d f_rep = Vector3d::Zero();
+    for(std::list<Vector3d>::iterator it = object_point.begin();
+            it != object_point.end(); it++) {
+        
+        Vector2d obj2 = Vector2d(it->x(), it->y());
+        Vector2d p2   = Vector2d(p_uav.x(), p_uav.y());
+            
+        double dist2 = (p2 - obj2).norm();
+        double dist =  (p_uav - *it).norm();
+        double c = (k_rep / (p2 - obj2).squaredNorm()) * pow(1/dist2 - 1/rng_infl, 1);
+        Vector3d f_temp;
+        f_temp << c * (p2 - obj2).normalized(), 0;
+        f_rep = f_rep + f_temp;
+    }
+    return f_rep;
+}
+
+/* TO DO: defines the direction of virtual point as orthogonal direction of drone */
+Vector3d getVirtualPoint(Vector3d p_uav) {
+    Vector3d f_virtual_point = Vector3d::Zero();
+
+    for(std::list<Vector3d>::iterator it = trapping_point.begin();
+            it != trapping_point.end(); it++) {
+        Vector2d obj2 = Vector2d(it->x(), it->y());
+        Vector2d p2   = Vector2d(p_uav.x(), p_uav.y());
+         
+        double dist2 = (p2 - obj2).norm();
+
+        if( dist2 <= rng_infl) {
+            Vector3d y = Vector3d(0,0,1);
+            Vector3d x = getFAtt(p).normalized();
+
+            Vector3d f_temp = 0.03 * k_rep * y.cross(x);
+            f_virtual_point = f_virtual_point - f_temp;
+        }
+    }
+
+    for(std::list<Vector3d>::iterator it = trapping_point.begin();
+            it != trapping_point.end(); it++) {
+        Vector2d obj2 = Vector2d(it->x(), it->y());
+        Vector2d p2   = Vector2d(p_uav.x(), p_uav.y());
+         
+        double dist2 = (p2 - obj2).norm();
+
+        if( dist2 > rng_infl/2) {
+            trapping_point.erase(it--);
+        }
+    }
+
+    return f_virtual_point;
+}
+
+Vector3d add_trapping_point() {
+    double max = 0;
+    Vector3d max_p;
+    for( std::list<Vector3d>::iterator it=skeleton_point.begin();
+          it != skeleton_point.end(); it++) {
+        Vector3d f_att = getFAtt(*it);
+        Vector3d f_rep = getFRep(*it);
+        double dot_prod = f_att.transpose() * (-f_rep);
+
+        if(dot_prod > max) {
+            max = dot_prod;
+            max_p = *it;
+        }
+    }
+    trapping_point.push_back(max_p);
+}
+
+void gaz_cb(const gazebo_msgs::ModelStates::ConstPtr& msg) { 
+    object_point.erase(object_point.begin(), object_point.end());
     for(int i=0; i< msg->name.size(); i++) {
         if((msg->name[i] != "ground_plane") && (msg->name[i] != "hummingbird")) {
             Vector3d obj = R_NED * Vector3d(msg->pose[i].position.x,
@@ -46,14 +126,10 @@ void gaz_cb(const gazebo_msgs::ModelStates::ConstPtr& msg) {
             double dist2 = (p2 - obj2).norm();
             double dist =  (p - obj).norm();
             if( dist2 <= rng_infl) {
-                double c = (k_rep / (p2 - obj2).squaredNorm()) * pow(1/dist2 - 1/rng_infl, 1);
-                Vector3d f_temp;
-                f_temp << c * (p2 - obj2).normalized(), 0;
-                f_rep = f_rep + f_temp;
+                object_point.push_back(obj);
             }
         }
     }
-    //ROS_ERROR_STREAM(f_rep.transpose());
 }
 
 void odom_cb(const nav_msgs::Odometry::ConstPtr& odom_msg) {
@@ -70,15 +146,31 @@ void odom_cb(const nav_msgs::Odometry::ConstPtr& odom_msg) {
     p_dot = RbNed*Vector3d( odom_msg->twist.twist.linear.x,
         odom_msg->twist.twist.linear.y, odom_msg->twist.twist.linear.z);
 
+    /* Creatin skeleton point */
+    skeleton_point.erase(skeleton_point.begin(),skeleton_point.end());
+    for(double i=0; i<2*M_PI; i=i+M_PI/4 ) {
+        Vector3d p_o = Vector3d( 0.5 * cos(i), 0.5 * sin(i), 0  );
+        skeleton_point.push_front(p + p_o);
+    }
+
+    /* Defining distance between desired position and actual position */
     Vector3d error = waypoints_list.front()-p;
     
     if(error.norm() < threshold & p_dot.norm() < 0.5 & (waypoints_list.size()>1)) 
         waypoints_list.pop_front();
 
-    //ROS_ERROR_STREAM("n_waypoints" << waypoints_list.size());
+    if(error.norm() > threshold & p_dot.norm() < 0.1) {
+        T_local = T_local + 1/rate;
+        if(T_local > 1) {
+            add_trapping_point();
+            T_local = 0;
+        }
+    } else {
+        T_local = 0;
+    }
     Vector3d position, speed, acceleration;
-    speed = error * ka / (error.norm() > 1 ? error.norm() : 1 );
-    speed = speed + f_rep;
+    
+    speed = getFAtt(p) + getFRep(p) + getVirtualPoint(p);
     acceleration = acceleration_filter.getDifferentiatoredValue(speed);
     position = p + speed/rate;
 
@@ -110,11 +202,14 @@ int main(int argc, char **argv)
     rng_infl    = nh.param<double>("/rng_infl", 1);
     k_rep       = nh.param<double>("/k_rep", 1);
 
+    T_local = 0;
+    flag_local_minimum = false;
+    
     acceleration_filter.DifferentiatorInit( kf_d, 1/rate );
 
     waypoints_list.push_back(Vector3d(0,0,-1));
-    waypoints_list.push_back(Vector3d(7,7,-2));
-    waypoints_list.push_back(Vector3d(7,7,-0.06));
+    waypoints_list.push_back(Vector3d(10,10,-2));
+    waypoints_list.push_back(Vector3d(10,10,-0.06));
 
     actual_point = waypoints_list.begin();
 
